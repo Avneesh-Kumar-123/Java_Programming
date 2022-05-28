3840826f0392dcb7e546561295e30dae08789c34
Avneesh <Avneesh@lenovo>
2022-05-28 13:34:15 +0530

    Commit

---
+++ java programming/src/unit_01/P1_Task01_FirstjavaProject.java
@@ -0,0 +1,10 @@
+package unit_01;
+
+public class P1_Task01_FirstjavaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world !!");
+	}
+
+}
